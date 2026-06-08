aeaae9f1fc3df8edd6c1ec6b8feee87a4e6d4511
Icahbod <usmanimamu17@gmail.com>
2026-06-08 11:44:58 +0000

    feat(wallet): validate Stellar public key format on input
    Add custom class-validator decorator that rejects invalid Stellar
    addresses using the stellar-sdk Address class.
    
    Closes #14

--- src/wallet/dto/wallet.dto.ts
+++ src/wallet/dto/wallet.dto.ts
@@ -1,7 +1,9 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { IsStellarPublicKey } from '../../common/validators/is-stellar-public-key.validator';
 
 export class UpsertWalletDto {
   @IsString()
   @IsNotEmpty()
+  @IsStellarPublicKey()
   publicKey: string;
 }
